2d41d914a2df80d5c3a650006d3b2b8b15a1660e
Thassanatou <hass.djire@gmail.com>
2020-01-03 00:05:21 +0100

    nettoyage mon scraping

--- scrap.py
+++ scrap.py
@@ -10,16 +10,16 @@ soup = b(html, "html.parser")
 file = open('bibliotheque_ile_de_france.csv', 'w+')
 writer = csv.writer(file)
 # le premier ligne
-writer.writerow(['DEPARTEMENT', 'VILLE', 'ADRESSE', 'E-MAIL'])
+writer.writerow(['DEPARTEMENT', 'VILLE', 'ADRESSE', 'CP' ,'E-MAIL'])
 #selectionne les données qui correspondent à la region îles de France par "id" puis par "li"
 for region in soup.find("div", {"id" : "bibliotheque_11_content"}):
 	for dep in region.findAll("li", {"class" : "lazy"}):
 		nom_departement = dep.div.a
 		dep_link = nom_departement['href']
 		departements = nom_departement.text
-			#print(" ")
-			#print(departements)
-			#0print(" ")
+		#print(" ")
+		#print(departements)
+		#print(" ")
 		# importer le deuxieme  url de tous les pages du departement	
 		url1 = "https://www.annuaire-mairie.fr" + dep_link
 		html1 = urllib.request.urlopen(url1).read()
@@ -36,14 +36,17 @@ for region in soup.find("div", {"id" : "bibliotheque_11_content"}):
 				#filtrer le "div" qui  correspond aux adresses du bibliotheque de la ville par  "ville_link"
 				bibliotheque = soup2.find("div",{"class": "ville_info"})
 				#on suprime le mot "Adresse" qui est devant les liens
-				adresse = bibliotheque.p.text.replace(" Adresse", "")
+				adresse = bibliotheque.p.text.replace(" Adresse", "").rpartition("\n")
+				rue = adresse[0].replace("\n", ", ")
+				CP = adresse[2].replace("(Le)", "").replace("(La)", "")
 				#filtrer tous les bibliotheques qui ont un adresse mail
 				for email in bibliotheque.findAll(text=re.compile("@")):
 					#print(departement)
-					#print(villes)
-					#print(adresse)
-					#print(email)
-					#print(" ")
-					writer.writerow([departements, villes, adresse, email])
+					print(villes)
+					print(rue)
+					print(CP)
+					print(email)
+					print(" ")
+					writer.writerow([departements, villes, rue, CP,  email])
 file.close()
 							
